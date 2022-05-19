d309ac5621a29cb9ba04bb38657a5e23f9eb3dc7
Cesar Miranda <cmirandac665@gmail.com>
2022-05-19 02:36:57 -0700

    Added: Support for blind signing

--- src/ui.h
+++ src/ui.h
@@ -14,6 +14,7 @@
 #include "os_io_seproxyhal.h"
 #include "bagl.h"
 
+
 /** the timer */
 extern int exit_timer;
 
@@ -28,6 +29,9 @@ extern char timer_desc[MAX_TIMER_TEXT_WIDTH];
 
 /** offset in the APDU header which says the length of the body. */
 #define APDU_BODY_LENGTH_OFFSET 4
+
+/** String Length */
+#define MESSAGE_SIZE_LEN_STRING (MESSAGE_SIZE_LEN*2)
 
 /** for signing, indicates this is the last part of the transaction. */
 #define P1_LAST 0x80
@@ -72,7 +76,22 @@ extern char timer_desc[MAX_TIMER_TEXT_WIDTH];
 
 /** UI currently displayed */
 enum UI_STATE {
-	UI_INIT, UI_IDLE, UI_TOP_SIGN, UI_TX_DESC_1,UI_TX_DESC_2, UI_SIGN, UI_DENY, UI_PUBLIC_KEY_1, UI_PUBLIC_KEY_2
+	UI_INIT, 
+	UI_IDLE, 
+	UI_IDLE_SETTINGS,
+	UI_TOP_SIGN, 
+	UI_TX_DESC_1,
+	UI_TX_DESC_2, 
+	UI_SIGN, UI_DENY, 
+	UI_PUBLIC_KEY_1, 
+	UI_PUBLIC_KEY_2, 
+	UI_TOP_BLIND_SIGNING, 
+	UI_BLIND_SIGNING_WARNING, 
+	UI_BLIND_SIGNING_REJECT, 
+	UI_BLIND_SIGNING_ACCEPT,
+	UI_BLIND_SIGNING_ENABLE_WARNING,
+	UI_BLIND_SIGNING_SETTINGS,
+	UI_BLIND_SIGNING_SETTING_GO_BACK
 };
 
 /** UI state enum */
@@ -105,6 +124,9 @@ extern unsigned char hash_data[HASH_DATA_SIZE];
 /** current length of hash. */
 extern unsigned int hash_data_ix;
 
+/** Is blind signing enabled */
+extern bool blind_signing_enabled_bool;
+
 /** all text descriptions. */
 extern char tx_desc[MAX_TX_TEXT_SCREENS][MAX_TX_TEXT_LINES][MAX_TX_TEXT_WIDTH];
 
@@ -117,11 +139,20 @@ extern char current_public_key[MAX_TX_TEXT_LINES][MAX_TX_TEXT_WIDTH];
 /** process a partial transaction */
 const bagl_element_t * io_seproxyhal_touch_approve(const bagl_element_t *e);
 
+/** sign a message */
+const bagl_element_t * io_seproxyhal_touch_approve2(const bagl_element_t *e);
+
 /** show the idle UI */
 void ui_idle(void);
 
 /** show the "Sign TX" ui, starting at the top of the Tx display */
 void ui_top_sign(void);
+
+/** show the "Blind Signing" ui, starting at the top of the blind signing display */
+void ui_top_blind_signing(void);
+
+/** show the "Blind signing must be enabled" flow */
+void ui_blind_singing_must_enable_message(void);
 
 /** return the length of the communication buffer */
 unsigned int get_apdu_buffer_length();
